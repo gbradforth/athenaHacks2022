5bdcef36add13f5ff534f6ba659a98166d2f1383
Gwen Bradforth <59940052+gbradforth@users.noreply.github.com>
2022-02-19 16:01:30 -0800

    Add files via upload

---
+++ AthenaHacks2022/ViewController.swift
@@ -0,0 +1,92 @@
+//
+//  ViewController.swift
+//  AthenaHacks2022
+//
+//  Created by Gwen Bradforth on 2/19/22.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    class Player{
+        private var tagged = false;
+        private var team = ""
+        private var hasFlag = false
+        
+        func Player(_ team: String)
+        {
+            self.team = team
+        }
+        
+        
+    }
+    let locationManager = CLLocationManager()
+
+    
+    @IBOutlet private var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+                    locationManager.delegate = self
+                    locationManager.startUpdatingLocation()
+                }
+        let initialLocation = CLLocation(longitude: location.coordinate.longitude, latitude: location.coordinate.latitude)
+        mapView.centerToLocation(initialLocation)
+        super.viewDidLoad()
+        let region = MKCoordinateRegion(
+          center: location.coordinate,
+          latitudinalMeters: 100,
+          longitudinalMeters: 100)
+        mapView.setCameraBoundary(
+          MKMapView.CameraBoundary(coordinateRegion: region),
+          animated: true)
+        
+        let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 200000)
+        mapView.setCameraZoomRange(zoomRange, animated: true)
+    }
+    
+    
+    
+
+    @IBAction func taggedButtonPressed(_ sender: UIButton) {
+        //player.tagged = true
+    }
+    
+    
+    
+    
+    func updateUI()
+    {
+        
+    }
+    
+    func locationManager(
+        _ manager: CLLocationManager,
+        didUpdateLocations locations: [CLLocation]
+    ) {
+        if let location = locations.last {
+            let latitude = location.coordinate.latitude
+            let longitude = location.coordinate.longitude
+            // Handle location update
+        }
+    }
+
+}
+
+private extension MKMapView {
+  func centerToLocation(
+    _ location: CLLocation,
+    regionRadius: CLLocationDistance = 1000
+  ) {
+    let coordinateRegion = MKCoordinateRegion(
+      center: location.coordinate,
+      latitudinalMeters: regionRadius,
+      longitudinalMeters: regionRadius)
+    setRegion(coordinateRegion, animated: true)
+  }
+}
