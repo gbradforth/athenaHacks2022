6fa6468c15fbf65ac9fdaa6688428fb6c13555dd
Gwen Bradforth <59940052+gbradforth@users.noreply.github.com>
2022-02-19 17:28:51 -0800

    Add files via upload

--- AthenaHacks2022/ViewController.swift
+++ AthenaHacks2022/ViewController.swift
@@ -10,51 +10,33 @@ import MapKit
 import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
-    class Player{
-        private var tagged = false;
-        private var team = ""
-        private var hasFlag = false
-        
-        func Player(_ team: String)
-        {
-            self.team = team
-        }
-        
-        
-    }
-    let locationManager = CLLocationManager()
-
     
-    @IBOutlet private var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
         self.locationManager.requestAlwaysAuthorization()
         self.locationManager.requestWhenInUseAuthorization()
         if CLLocationManager.locationServicesEnabled() {
                     locationManager.delegate = self
                     locationManager.startUpdatingLocation()
+                locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            
                 }
-        let initialLocation = CLLocation(longitude: location.coordinate.longitude, latitude: location.coordinate.latitude)
-        mapView.centerToLocation(initialLocation)
-        super.viewDidLoad()
-        let region = MKCoordinateRegion(
-          center: location.coordinate,
-          latitudinalMeters: 100,
-          longitudinalMeters: 100)
-        mapView.setCameraBoundary(
-          MKMapView.CameraBoundary(coordinateRegion: region),
-          animated: true)
         
-        let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 200000)
-        mapView.setCameraZoomRange(zoomRange, animated: true)
+        super.viewDidLoad()
+        
     }
     
     
     
 
     @IBAction func taggedButtonPressed(_ sender: UIButton) {
-        //player.tagged = true
+        //if (!inOwnTerritory)
+            //player.tagged = true
     }
     
     
@@ -62,7 +44,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     func updateUI()
     {
-        
+        //if player.tagged
+        // cannot access the QR code scanner
+        // perhaps have label that they have been tagged
+        //else
+        // display things normally
     }
     
     func locationManager(
@@ -73,20 +59,56 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             let latitude = location.coordinate.latitude
             let longitude = location.coordinate.longitude
             // Handle location update
+            mapView.showsUserLocation = true
+            mapView.centerToLocation(location)
         }
     }
 
 }
 
 private extension MKMapView {
-  func centerToLocation(
-    _ location: CLLocation,
-    regionRadius: CLLocationDistance = 1000
-  ) {
-    let coordinateRegion = MKCoordinateRegion(
-      center: location.coordinate,
-      latitudinalMeters: regionRadius,
-      longitudinalMeters: regionRadius)
-    setRegion(coordinateRegion, animated: true)
-  }
+    func centerToLocation(
+        _ location: CLLocation, //center point of region
+        regionRadius: CLLocationDistance = 1000
+    ) {
+        let coordinateRegion = MKCoordinateRegion(
+          center: location.coordinate,
+          latitudinalMeters: regionRadius,
+          longitudinalMeters: regionRadius)
+        setRegion(coordinateRegion, animated: true)
+    }
+}
+
+
+//modified from MapKit tutorial: https://www.raywenderlich.com/7738344-mapkit-tutorial-getting-started
+extension ViewController: MKMapViewDelegate {
+    /*
+    // mapView(_:viewFor:) gets called for every annotation you add to the map — like tableView(_:cellForRowAt:) when working with table views — to return the view for each annotation.
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        // make sure the annotation is a User obj
+        guard let annotation = annotation as? Player else {
+            return nil
+        }
+
+        let identifier = "artwork"
+        var view: MKMarkerAnnotationView
+        // map view reuses annotation views that are no longer visible.
+        // check to see if reusable annotation view is available before creating a new one
+        // When you dequeue a reusable annotation, you give it an identifier.
+        // If you have multiple styles of annotations, be sure to have a unique identifier for each one. it’s the same idea behind a cell identifier in tableView(_:cellForRowAt:).
+        if let dequeuedView = mapView.dequeueReusableAnnotationView(
+            withIdentifier: identifier) as? MKMarkerAnnotationView {
+            dequeuedView.annotation = annotation
+            view = dequeuedView
+        } else {
+            // create a new MKMarkerAnnotationView object if an annotation view could not be dequeued
+            view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            view.canShowCallout = true
+            view.calloutOffset = CGPoint(x: -5, y: 5)
+            view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        return view
+    }
+    */
+
 }
